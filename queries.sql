382be70a1054c1a6603a3fe76feab076ba55a8f7
Shedrack Sunday <Hannania002@gmail.com>
2022-12-06 16:51:45 +0100

    Add SQL query code

--- queries.sql
+++ queries.sql
@@ -1,3 +1,22 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * from animals WHERE name = 'Luna';
+
+SELECT * FROM animals WHERE name Like '%mon'
+
+--List the name of all animals born between 2016 and 2019.
+SELECT name FROM animals WHERE date_of_birth BETWEEN '2016-01-01' and '2019-12-31'
+
+
+SELECT name FROM animals WHERE neutered = true and escape_attempts < 3 ;
+
+-"Pikachu".
+SELECT date_of_birth FROM animals WHERE name IN('Agumon','Pikachu');
+
+
+SELECT name,escape_attempts from animals  WHERE weight_kg > 10.5;
+
+SELECT * from animals WHERE neutered = true;
+
+SELECT * FROM animals WHERE name NOT LIKE 'Gabumon';
+
+SELECT * from animals WHERE weight_kg >= 10.4 AND weight_kg<=17.3;

ba16dca1cb87a64a3443976870892a1d6bf17315
Shedrack Sunday <Hannania002@gmail.com>
2022-12-08 06:31:55 +0100

    Assign owner to each animal

--- data.sql
+++ data.sql
@@ -43,3 +43,11 @@ INSERT INTO species (name) VALUES ('Digimon');
 --Add species_id to all animals
 UPDATE animals SET species_id = 2 WHERE name LIKE '%mon%';
 UPDATE animals SET species_id = 1 WHERE species_id IS null;
+
+--Assign an owner to each animal
+
+UPDATE animals SET owner_id = 1 WHERE name = 'Agumon';
+UPDATE animals SET owner_id = 2 WHERE name = 'Gabumon' OR name = 'Pikachu';
+UPDATE animals SET owner_id = 3 WHERE name = 'Devimon' OR name = 'Plantmon';
+UPDATE animals SET owner_id = 4 WHERE name = 'Charmander' OR name = 'Squirtle' OR name = 'Blossom';
+UPDATE animals SET owner_id = 5 WHERE name = 'Angemon' OR name = 'Boarmon';
